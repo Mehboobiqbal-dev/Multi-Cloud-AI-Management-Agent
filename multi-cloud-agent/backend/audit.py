7ff0246737b430c27cbcaf82f365a8e84606a58f
Mehboob Iqbal <mehboobmehboob090@gmail.com>
2025-07-20 22:27:08 +0500

    Update audit.py

--- multi-cloud-agent/backend/audit.py
+++ multi-cloud-agent/backend/audit.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 Base = declarative_base()
-
+ 
 class AuditLog(Base):
     __tablename__ = 'audit_logs'
     id = Column(Integer, primary_key=True)
